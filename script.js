6726e6bcf93e6fb1a87230b90db9c610e09626ec
egormozg <96963687+egormozg@users.noreply.github.com>
2022-07-16 15:32:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(function () {
     $(".deeplink").click(function (e) {
     e.preventDefault();
-      // Detect device
-    var devType = getMobileOperatingSystem();
+//       Detect device
+    let devType = getMobileOperatingSystem();
 
-    var android_apk_url = $(this).attr('android-apk');
-    var apk_url = $(this).attr('apk');
-    var siteUrl = $(this).attr('href');
+    let android_apk_url = $(this).attr('android-apk');
+    let apk_url = $(this).attr('apk');
+    let siteUrl = $(this).attr('href');
 
-      // alert(url)
+//       alert(url)
     switch (devType) {
         case 'android':
         $('#app-frame').attr('src', android_apk_url);        
@@ -30,22 +30,22 @@ $(function () {
         break;
     }
 
-      // window.location = url;
-      // setTimeout(function () {
-      //   window.location = url;
-      // }, 500);
+       window.location = url;
+       setTimeout(function () {
+         window.location = url;
+       }, 500);
     });
 
 });
-var getMobileOperatingSystem = function () {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+let getMobileOperatingSystem = function () {
+    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    // Windows Phone must come first because its UA also contains "Android"
+//     Windows Phone must come first because its UA also contains "Android"
     if (/android/i.test(userAgent)) {
     return "android";
     }
 
-    // iOS detection from: https://stackoverflow.com/a/9039885/177710
+//     iOS detection from: https://stackoverflow.com/a/9039885/177710
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     return "ios";
     }
